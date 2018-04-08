f6337fbc7b95dc87dc82c4641f45b8265ddc5c7b
Justin Cox <misterjustincox@gmail.com>
2018-04-08 07:47:12 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space != " "
+      counter += 1
+    end
+  end
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "O" : "X"
+end
